15628aa4af7ef43ef9f962aa79565d3268081aae
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-06-09 13:36:07 +0530

    update

--- Routes/note.routes.js
+++ Routes/note.routes.js
@@ -10,23 +10,27 @@ notesRouter.post("/create", async (req, res) => {
   try {
     const note = new NoteModel(req.body);
     await note.save();
-    res.json({ msg: "New note has been added", note });
+    res.json({ msg: "Note saved!", note });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error });
   }
 });
 
 notesRouter.get("/", async (req, res) => {
+  const { userID } = req.body;
+
   try {
-    const notes = await NoteModel.find({ userID: req.body.userID });
+    const notes = await NoteModel.find({ userID });
     res.json({ notes });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error });
   }
 });
 
 notesRouter.patch("/update/:noteID", async (req, res) => {
-  const { title, body, category } = req.body;
+  const { noteID } = req.params;
+  const { userID, title, body, category } = req.body;
+
   const query = {};
 
   if (title) {
@@ -42,37 +46,32 @@ notesRouter.patch("/update/:noteID", async (req, res) => {
   }
 
   try {
-    let note = await NoteModel.findOneAndUpdate(
-      {
-        _id: req.params.noteID,
-        userID: req.body.userID,
-      },
-      query //or req.body;
-    );
-    if (note) {
-      res.json({ msg: "Note update!", note });
+    const note = await NoteModel.findById({ _id: noteID });
+    if (note && note.userID == userID) {
+      await NoteModel.findByIdAndUpdate({ _id: noteID }, query);
+      res.json({ msg: `${note.title} has been updated.` });
     } else {
-      res.json({ msg: "Note update unsuccessful." });
+      res.json({ msg: "Not Authorised." });
     }
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error });
   }
 });
 
 notesRouter.delete("/delete/:noteID", async (req, res) => {
+  const { noteID } = req.params;
   try {
-    const deleted = await NoteModel.findOneAndDelete({
-      _id: req.params.noteID,
+    const note = await NoteModel.findOneAndDelete({
+      _id: noteID,
       userID: req.body.userID,
     });
-
-    if (deleted) {
-      res.json({ msg: "Note deleted!" });
+    if (note) {
+      res.json({ msg: "Note has been deleted.", note });
     } else {
-      res.json({ msg: "Note deletion was unsuccessful." });
+      res.json({ msg: "Not Authorised." });
     }
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error });
   }
 });
 
